983018f33675ca7997e0a3272e286a539ae43251
beezontop <d1882002@gmail.com>
2020-07-07 21:33:54 +0800

    sm

--- js/main.js
+++ js/main.js
@@ -4,7 +4,32 @@ $('document').ready(function(){
         $('#pop-up').addClass('d-block');
         $(e.target).preventDefault();
     });
- 
+
+
+    $('#new-container').mouseenter(function(e){
+        $('#left-top').animate({"left": "-900px"},1000,function(e){
+            $('#left-top').addClass('d-none');
+        });
+        $('#right-top').animate({"top": "-900px"},1000,function(e){
+            $('#right-top').addClass('d-none');
+        });
+        $('#right-bottom').animate({"right": "-900px"},1000,function(e){
+            $('#right-bottom').addClass('d-none');
+        });
+        $('#left-bottom').animate({"top": "900px"},1000,function(e){
+            $('#left-bottom').addClass('d-none');
+        });
+    });
+
+    $('#new-container').mouseleave(function(e){
+
+        $('#left-top,#right-top,#right-bottom,#left-bottom').removeClass('d-none');
+        $('#left-top').animate({"left": "0"},1000);
+        $('#right-top').animate({"top": "0"},1000);
+        $('#right-bottom').animate({"right": "0"},1000);
+        $('#left-bottom').animate({"top": "50%"},1000);
+    });
+    
     function windowSize() {
         // let width=$(window).width()+"px";
         let height=$(window).height()+"px";
@@ -15,5 +40,6 @@ $('document').ready(function(){
     });
     windowSize();
 
+   
 
 });
